e64210151b70ead1de5e43f03b51eb22a9ac0c31
Max <48467432+Max-Frisch@users.noreply.github.com>
2022-03-24 22:52:47 +0700

    Create TicTacToe.java

---
+++ src/com/company/jetbrainsAcademy/tempPrograms/TicTacToe.java
@@ -0,0 +1,16 @@
+package Java001.src.com.company.jetbrainsAcademy.tempPrograms;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            System.out.println();
+            for (int j = 0; j < 3; j++) {
+                if (j == 1) {
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O ");
+                }
+            }
+        }
+    }
+}
