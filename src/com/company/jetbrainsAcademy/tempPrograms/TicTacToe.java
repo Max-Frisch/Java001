393fd3f2653e987671f5cbe34fe7c67cff3c7506
Max <48467432+Max-Frisch@users.noreply.github.com>
2022-03-25 14:21:10 +0700

    Update TicTacToe.java

--- src/com/company/jetbrainsAcademy/tempPrograms/TicTacToe.java
+++ src/com/company/jetbrainsAcademy/tempPrograms/TicTacToe.java
@@ -2,6 +2,7 @@ package Java001.src.com.company.jetbrainsAcademy.tempPrograms;
 
 public class TicTacToe {
     public static void main(String[] args) {
+
         for (int i = 0; i < 3; i++) {
             System.out.println();
             for (int j = 0; j < 3; j++) {
